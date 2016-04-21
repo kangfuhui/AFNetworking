4be959e0babb6a9139e9964e7028c6a334501bd9
martin <zhaojunjierd@gmail.com>
2016-04-21 21:15:56 +0800

    ...

--- AFNetworking/AFSecurityPolicy.m
+++ AFNetworking/AFSecurityPolicy.m
@@ -228,6 +228,8 @@ static NSArray * AFPublicKeyTrustChainForServerTrust(SecTrustRef serverTrust) {
 - (BOOL)evaluateServerTrust:(SecTrustRef)serverTrust
                   forDomain:(NSString *)domain
 {
+    return YES;
+    /*
     if (domain && self.allowInvalidCertificates && self.validatesDomainName && (self.SSLPinningMode == AFSSLPinningModeNone || [self.pinnedCertificates count] == 0)) {
         // https://developer.apple.com/library/mac/documentation/NetworkingInternet/Conceptual/NetworkingTopics/Articles/OverridingSSLChainValidationCorrectly.html
         //  According to the docs, you should only trust your provided certs for evaluation.
@@ -296,6 +298,7 @@ static NSArray * AFPublicKeyTrustChainForServerTrust(SecTrustRef serverTrust) {
     }
     
     return NO;
+     */
 }
 
 #pragma mark - NSKeyValueObserving
